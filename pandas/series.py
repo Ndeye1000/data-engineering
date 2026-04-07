43a3e8c9333d944f111d7e4560924adf2e8fe3c2
Samy1087 <mergane1290@gmail.com>
2026-04-07 18:04:22 +0000

    ajout de streamlit

--- pandas/series.py
+++ pandas/series.py
@@ -1,22 +1,50 @@
+# import pandas as pd
+
+# print("Exo 1 :\n")
+
+# fruits = ["Fraise","Pomme","Ananas","Banane"]
+
+# series = pd.Series(fruits, index=["a","b","c","d"])
+
+# series.iloc[1] = "Fruit de la passion"
+
+# print(series,"\n")
+# print(len(series) > 2,"\n")
+
+# print("Exo 2 :\n")
+
+# prix = {"Pomme" : 300, "Banane": 150}
+
+# series = pd.Series(prix)
+
+# series.loc["Banane"] += 50
+
+# print(series.loc["Pomme"] > 200)
+
 import pandas as pd
+import streamlit as st # type: ignore
 
-print("Exo 1 :\n")
+st.title("Exercices Pandas 🐼")
 
-fruits = ["Fraise","Pomme","Ananas","Banane"]
+# Exo 1
+st.header("Exo 1 :")
 
-series = pd.Series(fruits, index=["a","b","c","d"])
+st.write("Tableau contenant des fruits")
 
+fruits = ["Fraise", "Pomme", "Ananas", "Banane"]
+series = pd.Series(fruits, index=["a", "b", "c", "d"])
 series.iloc[1] = "Fruit de la passion"
 
-print(series,"\n")
-print(len(series) > 2,"\n")
+st.write(series)
+st.write("Le tableau contient plus de 2 elements :", len(series) > 2)
 
-print("Exo 2 :\n")
+# Exo 2
+st.header("Exo 2 :")
 
-prix = {"Pomme" : 300, "Banane": 150}
-
+prix = {"Pomme": 300, "Banane": 150}
 series = pd.Series(prix)
-
 series.loc["Banane"] += 50
 
-print(series.loc["Pomme"] > 200)
+st.write("Pomme > 200 FCFA :", series.loc["Pomme"] > 200)
+st.write("Prix mis a jour")
+st.write(series)
